0f879370d159acb2bb2bdb39749bf7a34e5b0f2d
Nicol Acosta <nicolacosta8@gmail.com>
2023-10-16 11:32:17 -0600

    feat: added hexdocs lib

--- mix.exs
+++ mix.exs
@@ -7,7 +7,16 @@ defmodule FlEx.MixProject do
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "fl_ex",
+      description: "Lightweight and easy to use API micro-framework.",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      package: package(),
+      source_url: "https://github.com/nicolkill/fl_ex",
+      docs: [
+        extras: ["README.md"]
+      ]
     ]
   end
 
@@ -21,12 +30,20 @@ defmodule FlEx.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:plug, "~> 1.14"},
       {:plug_cowboy, "~> 2.0"},
       {:httpoison, "~> 2.0", only: :test, runtime: false},
       {:jason, "~> 1.2"}
+    ]
+  end
+
+  defp package do
+    [
+      name: "fl_ex",
+      files: ~w(lib .formatter.exs mix.exs README*),
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/nicolkill/fl_ex"},
     ]
   end
 end
